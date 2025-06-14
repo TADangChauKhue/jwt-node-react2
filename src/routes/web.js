e5c23454d0fb29023bd7dee5f00fa387d9975d8d
TA Dang Chau Khue <khue118@gmail.com>
2025-06-14 17:14:57 +0200

    video4 - usebootstrap5

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,8 +5,9 @@ import express from "express";
 import homeController from '../controller/homeController';
 const router = express.Router();
 const initWebRoutes=(app)=>{
+    //path, handler
     router.get("/", homeController.handleHelloWord);
-    router.get("/user", homeController.handleUserPage)    
+    router.get("/user", homeController.handleUserPage);    
     return app.use("/",router);
 }
 
