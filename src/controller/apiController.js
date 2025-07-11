18f08fa2993c03f08d71a09ca0348be5220b66b3
TA Dang Chau Khue <khue118@gmail.com>
2025-07-11 13:41:19 +0200

    vid 10.2.1

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,4 +1,5 @@
 
+import loginRegisterService from '../service/loginRegisterService';
 const testApi=(req,res) => {
     return res.status(200).json({
         message:'ok',
@@ -7,8 +8,41 @@ const testApi=(req,res) => {
 
 }
 
-const handleRegister=(req,res)=>{
-    console.log(">>> call me", req.body)
+const handleRegister=async(req,res)=>{
+    try{
+        // req.body: email, phone, username, password
+        if(!req.body.email || !req.body.password){
+            return res.status(200).json({
+            EM: 'Missing required parameters', // error message
+            EC:'1',//error code
+            DT: '',//date
+            })
+    
+        }
+        if(req.body.password && req.body.password.length <3){
+            return res.status(200).json({
+            EM: 'Your password must have more than 3 letters', // error message
+            EC:'1',//error code
+            DT: '',//date
+            })
+        }
+
+        // service: create user
+
+        let data=await loginRegisterService.registerNewUser(req.body)
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,//error code
+            DT: '',//date
+        }) 
+
+    }catch(e){
+        return res.status(500).json({
+            EM: 'error',
+            EC:'-1',//error code
+            DT: '',//date
+        })
+    }
 
 }
 module.exports = {
