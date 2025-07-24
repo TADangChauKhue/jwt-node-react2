1c315ab68b640c9b4f67938cddbe101d8f699c0f
TA Dang Chau Khue <khue118@gmail.com>
2025-07-24 18:09:38 +0200

    vide 10.6.3

--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -35,6 +35,8 @@ const getUserWithPagination = async(page,limit) => {
         const {count,rows} = await db.User.findAndCountAll({
             offset: offset,
             limit: limit,
+            attributes:["id","username","email", "phone", "sex"],
+            include:{model:db.Group, attributes:["name","description"]},
         })
         let totalPages = Math.ceil(count/limit);
         let data ={
@@ -96,11 +98,32 @@ const updateUser =async(data)=>{
 
 const deleteUser =async(id)=>{
     try {
-        await db.User.delete({
+        let user = await db.User.findOne({
             where:{id:id}
         })
+        if (user){
+            await user.destroy();
+            return{
+                EM:'Delete user suceeds',
+                EC:0,
+                DT:[]
+                } 
+
+        }else{
+            return{
+                EM:'User not exist',
+                EC:2,
+                DT:[]
+                } 
+
+        }
     } catch(e){
         console.log(e);
+            return{
+                EM:'error from service',
+                EC:-1,
+                DT:[]
+                } 
     }
 
 }
